d9d02b03b4d86ed5d5ce6032acd20bec82b918f5
Jeremy Ward <exochos@gmail.com>
2022-04-12 23:16:30 -0700

    Updated with tests

---
+++ app/src/androidTest/java/com/example/ad340jeremywardweeklyassignments/MainActivityTest.java
@@ -0,0 +1,31 @@
+package com.example.ad340jeremywardweeklyassignments;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static  androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.junit.Assert.*;
+
+import android.app.Activity;
+
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> activitytestRule
+            = new ActivityScenarioRule<>(MainActivity.class);
+
+    @Test
+    public void hasTextOnScreen() {
+        onView(withId(R.id.helloworld))
+                .check(matches(withText(R.string.hello_world)));
+    }
+}

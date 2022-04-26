1143c0319c993eb9e58721ef49935d62869776ea
Jeremy Ward <exochos@gmail.com>
2022-04-26 11:01:18 -0700

    Updated for Circle

--- app/src/main/java/com/example/ad340jeremywardweeklyassignments/MainActivity.java
+++ app/src/main/java/com/example/ad340jeremywardweeklyassignments/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.ad340jeremywardweeklyassignments;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText nameField = (EditText) findViewById(R.id.name);
+    private EditText emailAddressField;
+    private EditText usernameField;
+    private EditText descriptionField;
+    private EditText occupationField;
+    private TextView dobTextView;
+    private int dobYear = 0;
+    private int dobMonth = 0;
+    private int dobDay = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goToNextActivity(View view) {
+        Intent intent = new Intent();
+        intent.putExtra(Constant.KEY_NAME, "Test Value");
+        intent.putExtra(String.valueOf(nameField), "New Name");
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/ad340jeremywardweeklyassignments/SecondActivity.java
@@ -0,0 +1,6 @@
+package com.example.ad340jeremywardweeklyassignments;
+
+import android.app.Activity;
+
+public class SecondActivity extends Activity {
+}

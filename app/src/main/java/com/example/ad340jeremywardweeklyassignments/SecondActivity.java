bcbfc956150db860b9f76ec9684a824ab16aff4c
Jeremy Ward <exochos@gmail.com>
2022-04-30 20:38:47 -0700

    Stuff
    Stuff

--- app/src/main/java/com/example/ad340jeremywardweeklyassignments/SecondActivity.java
+++ app/src/main/java/com/example/ad340jeremywardweeklyassignments/SecondActivity.java
@@ -1,6 +1,25 @@
 package com.example.ad340jeremywardweeklyassignments;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SecondActivity extends Activity {
+    Button button;
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        button = (Button)findViewById(R.id.goToAnotherActivity);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(SecondActivity.this, MainActivity.class);
+                SecondActivity.this.startActivity(myIntent);
+            }
+        });
+    }
 }
